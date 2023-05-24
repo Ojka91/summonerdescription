5e70945ccb42feeaa72ae4dfcd6462560d95854d
Oscar Lucena Papiol <oscar@ip-192-168-18-14.eu-west-1.compute.internal>
2023-05-24 10:49:28 +0200

    Versus starting..

--- src/controllers/generate_free_versus.ts
+++ src/controllers/generate_free_versus.ts
@@ -33,6 +33,19 @@ export default class GenerateFreeVersus {
             // Get prompt for free tier
             const prompt = freePrompt.getVersusPrompt()
 
+            /**
+             * TODO !!! for image and champ generation
+             * Based on this data from league of legends game:
+{"playerRank":"Player is rank SILVER II with 39 wins and 56 losses","name":"Ojka","isInHotStreak":false,"gamesInfo":["Player played CLASSIC game type. Player lost the game with 14/7/6 playing Akali MIDDLE","Player played ARAM game type. Player lost the game with 9/12/16 playing Yasuo ","Player played CLASSIC game type. Player lost the game with 6/8/3 playing Akali MIDDLE","Player played CLASSIC game type. Player lost the game with 11/10/5 playing Akali MIDDLE","Player played ARAM game type. Player lost the game with 12/13/18 playing Kassadin ","Player played ARAM game type. Player lost the game with 12/16/23 playing Maokai ","Player played ARAM game type. Player lost the game with 7/13/34 playing Ahri ","Player played CLASSIC game type. Player won the game with 8/7/10 playing Akali MIDDLE","Player played ARAM game type. Player lost the game with 14/15/36 playing Lissandra ","Player played CLASSIC game type. Player lost the game with 5/8/0 playing Akali MIDDLE","Player played ARAM game type. Player lost the game with 15/16/25 playing Fizz ","Player played CLASSIC game type. Player lost the game with 10/12/9 playing Lux MIDDLE","Player played CLASSIC game type. Player won the game with 16/7/8 playing Akali MIDDLE","Player played CLASSIC game type. Player lost the game with 5/7/7 playing MonkeyKing JUNGLE","Player played ARAM game type. Player won the game with 6/10/34 playing Anivia "]}
+
+Create a new champion League of legends champion. Be creative
+Generate the result in a json following this example
+{
+    "champion": "", // here it goes all the description of the champion as string, spells, abilities...
+    "appearance": "" // here it goes ONLY the appearance description,as string maximum 100 charactes
+}
+             */
+
             return await chatGpt.chat(`${prompt} ${metadata1} ${metadata2}`, openAiApiKey)
 
         } catch (error) {
